0a0169f2ee9f5d7938d29b562b1e3280a6e04d72
Nayeel Imtiaz <naimtiaz@ucsc.edu>
2022-02-28 22:02:08 -0800

    Void.c

--- kr/ch1/void.c
+++ kr/ch1/void.c
@@ -5,7 +5,9 @@ int main(void)
 	printf("%lu\n", sizeof(int));
     int a[2] = {1, 2};
     void *ptr = &a;
+    printf("%p\n", ptr);
     ptr = ptr + sizeof(int);	// void is considered to be size of 1 byte here.
-    printf("%d", *(int *)ptr);
+    printf("%p\n", ptr);
+    printf("%d",  * (int *) ptr);
     return 0;
 }
